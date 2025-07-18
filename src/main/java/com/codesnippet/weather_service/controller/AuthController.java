010c3a04a46fd7b71bd3dddbe02678ece65ec400
Rahul Sharma <rahul.sharma@incture.com>
2025-07-18 20:02:24 +0530

    working on jwt

--- src/main/java/com/codesnippet/weather_service/controller/AuthController.java
+++ src/main/java/com/codesnippet/weather_service/controller/AuthController.java
@@ -7,22 +7,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.codesnippet.weather_service.entity.AuthRequest;
+import com.codesnippet.weather_service.util.JWTUtil;
 
 @RestController
 public class AuthController {
     
     @Autowired
     private AuthenticationManager authenticationManager;
-    
+    @Autowired
+    private JWTUtil jwtUtil;
     @PostMapping("/authenticate")
     public String generateToken(@RequestBody AuthRequest authRequest){
         try{
+            // Authenticate the user
             authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+            // If authentication is successful, generate and return the JWT token
+            // The JWTUtil class should have a method to generate the token
+            return jwtUtil.generateToken(authRequest.getUsername());
         }
         catch(Exception e) {
             throw new RuntimeException("Invalid username or password");
         }
-        return "GeneratedToken"; 
+        
     }
 }
